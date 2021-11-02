4db8a533abdf7f96a9bacb92dcfb88d931a2dd9d
t_warsop <tewarsop@gmail.com>
2021-11-02 14:22:48 +0000

    Implemented the inject many feature.
    Using the other me page as a test.

--- src/scaffss.py
+++ src/scaffss.py
@@ -17,15 +17,29 @@ class PageFile():
     def fullpath(self):
         return os.path.join(self.location, self.name)
 
-class Page():
-    def __init__(self, page_file: PageFile, inject_files: List[Dict[str, str]], inject_literals: List[Dict[str, str]]):
+class InjectMany():
+    def __init__(self, tag: str, page_file: PageFile, inject_literal_sets: List[Dict[str, str]]):
+        self.tag = tag
         self.page_file = page_file
-        self.inject_files = inject_files
-        self.inject_literals = inject_literals
+        self.inject_literal_sets = inject_literal_sets
 
     @classmethod
     def from_json(cls, data):
-        return cls(PageFile.from_json(data["page_file"]), data["inject_files"], data["inject_literals"])
+        return cls(data['tag'], PageFile.from_json(data['page_file']), data['inject_literal_sets'])
+
+class Page():
+    def __init__(self, page_file: PageFile, inject_files: List[Dict[str, str]], inject_literals: List[Dict[str, str]], inject_many: List[InjectMany]):
+        self.page_file = page_file
+        self.inject_files = inject_files
+        self.inject_literals = inject_literals
+        self.inject_many = inject_many
+
+    @classmethod
+    def from_json(cls, data):
+        inject_many = []
+        for im in data['inject_many']:
+            inject_many.append(InjectMany.from_json(im))
+        return cls(PageFile.from_json(data["page_file"]), data["inject_files"], data["inject_literals"], inject_many)
 
 class Scaffss():
     def __init__(self, output_folder: str, static_folder: str, pages: List[Page]):
@@ -64,6 +78,16 @@ def build(scaffss_file_location):
         for inject_literal in page.inject_literals:
             for key, value in inject_literal.items():
                 page_inject_contents[key] = value
+
+        for im in page.inject_many:
+            im_page = read_file(im.page_file.fullpath())
+
+            im_contents = ''
+            for ils in im.inject_literal_sets:
+                print(ils)
+                im_contents += Template(im_page).render(**ils)
+
+            page_inject_contents[im.tag] = im_contents
 
         with open(os.path.join(scaffss.output_folder, page.page_file.name), 'w') as output_file:
             previously_rendered_template = None
